5b9cbe52c748f9b9b38fd23a4cce1a2497bbcc8e
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-19 10:23:33 +0100

    Added Database settings for Postgres

--- ShopNest_api/ShopNest_api/settings.py
+++ ShopNest_api/ShopNest_api/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'ShopNest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_db',
+        'USER': 'postgres',
+        'PASSWORD': 'decima001@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
