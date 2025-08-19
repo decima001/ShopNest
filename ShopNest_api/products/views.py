abf0c55adb005053ae57b0ee29c9897e89628fde
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-19 10:45:49 +0100

    Added import JSON as productlist in products/views.py

--- ShopNest_api/products/views.py
+++ ShopNest_api/products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+def product_list(request):
+    return JsonResponse({"message": "List of products"})
 
 # Create your views here.
