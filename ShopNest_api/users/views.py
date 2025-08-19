c919b3b289f4c5ef77c4b623476298465111ba1c
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-19 10:49:27 +0100

    Added import JSON for views in users/views.py

--- ShopNest_api/users/views.py
+++ ShopNest_api/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+def user_list(request):
+    return JsonResponse({"message": "List of users"})
 
 # Create your views here.
