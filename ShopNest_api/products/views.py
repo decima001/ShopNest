9e3aadac133f5cb0ac7af4fbbc0bac227bc767cb
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-30 13:19:31 +0100

    Added product list for views.py in products

--- ShopNest_api/products/views.py
+++ ShopNest_api/products/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import generics, filters
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 def product_list(request):
     return JsonResponse({"message": "List of products"})
 
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'price', 'stock_quantity']
+    search_fields = ['name', 'category__name']
+    pagination_class = None  # Pagination is set in settings.py
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
 # Create your views here.
