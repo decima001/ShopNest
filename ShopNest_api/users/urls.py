bab56dbd232f30b286bf585e5b15bb98b7c4237c
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-30 13:25:40 +0100

    Updated users/urls.py

--- ShopNest_api/users/urls.py
+++ ShopNest_api/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import UserListCreateView, UserDetailView
 
 urlpatterns = [
-    path('', views.user_list, name='user-list'),
+    path('', UserListCreateView.as_view(), name='user-list-create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
