a597fc599f9102cf94c53579ddc8beca97237900
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-30 13:22:10 +0100

    Updates views.py in Users Views

--- ShopNest_api/users/views.py
+++ ShopNest_api/users/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 def user_list(request):
     return JsonResponse({"message": "List of users"})
 
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 # Create your views here.
