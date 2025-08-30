e257d836de2559cb5fd8e40f4d238aaee7301075
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-30 12:33:55 +0100

    Updated settings.py and users/models.py

--- ShopNest_api/ShopNest_api/settings.py
+++ ShopNest_api/ShopNest_api/settings.py
@@ -70,10 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ShopNest_api.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -86,7 +82,6 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
